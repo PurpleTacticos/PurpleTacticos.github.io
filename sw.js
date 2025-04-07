1266d44145568434a78ecddf7f4bdaef354f563b
PurpleTacticos <meowwnewney@gmail.com>
2025-04-08 03:33:40 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,50 @@
-const CACHE_NAME = 'v2';
+// sw.js
+const CACHE_VERSION = 'v3';
+const OFFLINE_PAGE = '/offline.html';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/search.html',
-  '/legal.html',
-  '/styles/main.css',
-  '/scripts/app.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/dompurify/3.0.5/purify.min.js'
+    '/',
+    '/index.html',
+    '/all-takes.html',
+    '/search.html',
+    '/legal.html',
+    '/styles/main.css',
+    '/scripts/app.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/dompurify/3.0.5/purify.min.js'
 ];
 
-// Install Event
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-  );
+    e.waitUntil(
+        caches.open(CACHE_VERSION)
+            .then(cache => cache.addAll(ASSETS))
+    );
 });
 
-// Fetch Event
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(res => res || fetch(e.request))
-  );
+    e.respondWith(
+        caches.match(e.request)
+            .then(cachedResponse => {
+                const fetchPromise = fetch(e.request).then(networkResponse => {
+                    const cacheCopy = networkResponse.clone();
+                    caches.open(CACHE_VERSION).then(cache => {
+                        cache.put(e.request, cacheCopy);
+                    });
+                    return networkResponse;
+                }).catch(() => {
+                    if (e.request.mode === 'navigate') return caches.match(OFFLINE_PAGE);
+                    return new Response('Offline');
+                });
+                return cachedResponse || fetchPromise;
+            })
+    );
 });
 
-// Activate Event
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
+    e.waitUntil(
+        caches.keys().then(keys => 
+            Promise.all(
+                keys.filter(key => key !== CACHE_VERSION)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
 });
